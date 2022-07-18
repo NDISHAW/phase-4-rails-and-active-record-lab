764564766b4ab005ecfdf6fa7bc8e74b47708e7e
NDICHU <ndichumuriithi@gmail.com>
2022-07-18 14:50:58 +0300

    Completed assignment

---
+++ .history/app/models/student_20220718144750.rb
@@ -0,0 +1,5 @@
+class Student < ApplicationRecord
+    def to_S
+        Student.all
+    end
+end
---
+++ .history/app/models/student_20220718144806.rb
@@ -0,0 +1,5 @@
+class Student < ApplicationRecord
+    def to_S
+        S"#{first_na}"
+    end
+end
